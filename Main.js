10cb299f1ac79fd62e81153fd91bc0972547b76a
neeln4j <neel.jaya@a-cti.com>
2016-05-20 17:39:01 +0530

    some changes

--- Main.js
+++ Main.js
@@ -4,12 +4,12 @@ const BrowserWindow = electron.BrowserWindow;
 var app = require('app');
 var mainWindow = null;
 
-console.log("main.js",'file://', __dirname, "index.html");
 app.on('ready', function() {
     mainWindow = new BrowserWindow();
     mainWindow.loadURL(path.join('file://', __dirname, "index.html"));
 });
 
 app.on('window-all-closed',function(){
+	console.log("window-all-closed event trigerred.")
 	app.quit();
 });
